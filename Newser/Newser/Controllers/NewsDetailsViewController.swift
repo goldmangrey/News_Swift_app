edb239dd96adc0e6ac575ce2ed0d53880d4d3377
Yeskendir <91181514+goldmangrey@users.noreply.github.com>
2022-04-10 04:33:22 +0600

    Add files via upload

---
+++ Newser/Newser/Controllers/NewsDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewsDetailsViewController.swift
+//  Newser
+//
+//  Created by Yeskendir on 01.03.2022.
+//
+
+import UIKit
+
+class NewsDetailsViewController: UIViewController {
+
+    @IBOutlet weak var newImage: UIImageView!
+    @IBOutlet weak var newTitle: UILabel!
+    @IBOutlet weak var newDescription: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
